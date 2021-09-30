55f074f8795906c9b5cfad3463686a933cf81aac
Dylan Cherry <dylancherry98@gmail.com>
2021-09-30 12:08:30 -0400

    Fixed alignment

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ function App() {
     <body class="md:text bg-gray-800">
       <div class="p-16 text-center">
         <h1 class="text-4xl text-red-400">Dylan Cherry</h1>
-        <div class="md:flex space-x-4">
-          <div class="text-left">
-            <p class="pt-12 font-bold text-white">Hi, I am Dylan Cherry. I am an aspiring full-stack developer living in Montreal, Canada. I recently graduated Le Wagon Montreal's full-time intensive web development bootcamp. I am interested in full-time work.</p>
+        <div class="md:flex space-x-4 pt-12 ">
+          <div class="text-left pr-48 space-y-24">
+            <p class="font-bold text-white">Hi, I am Dylan Cherry. I am an aspiring full-stack developer living in Montreal, Canada. I recently graduated Le Wagon Montreal's full-time intensive web development bootcamp. I am interested in full-time work.</p>
             <button class="mt-12 bg-red-400 hover:bg-gray-400 text-white font-bold py-2 px-4 rounded">Contact me</button>
           </div>
           <div>
