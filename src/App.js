b57fe327023fdc5f97af7f72c67511db212dc123
Dylan Cherry <dylancherry98@gmail.com>
2021-09-30 12:58:10 -0400

    Changed image width, added border.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
         <h1 class="text-5xl text-red-400">Dylan Cherry</h1>
         <div class="md:flex space-x-4 pt-12">
           <div>
-            <div class="flex flex-wrap md:items-center pb-12">
-              <img src="https://res.cloudinary.com/dploqe2ts/image/upload/v1633016623/T02NE0241-U026Z2X2YV7-a31615815abe-512_tfvvjs.png" alt="Portrait of Dylan Cherry" class="rounded-md mx-auto"></img>
+            <div class="md:items-center pb-12 w-200px">
+              <img class="w-64 rounded-md mx-auto border-4 border-red-400" src="https://res.cloudinary.com/dploqe2ts/image/upload/v1633016623/T02NE0241-U026Z2X2YV7-a31615815abe-512_tfvvjs.png" alt="Portrait of Dylan Cherry"></img>
             </div>
             <p class="font-bold text-white">Hi, I am Dylan Cherry. I am an aspiring full-stack developer living in Montreal, Canada. I recently graduated Le Wagon Montreal's full-time intensive web development bootcamp. I am interested in full-time work.</p>
             <button class="mt-12 bg-red-400 hover:bg-gray-400 text-white font-bold py-2 px-4 rounded">Contact me</button>
@@ -19,7 +19,7 @@ function App() {
         <div class="text-center mb-12">
           <h2 class="text-4xl text-red-400 font-bold">About Me</h2>
         </div>
-        <div class="text-white border-b-2 border-red-400 pb-12">
+        <div class="text-white border-b-4 border-red-400 pb-12">
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?</p>
